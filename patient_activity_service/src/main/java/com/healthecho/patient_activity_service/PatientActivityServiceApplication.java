25ae48fa6befe874e2d225936dd2fcd31c7f5a7a
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-11-30 11:42:37 +0600

    patient activity services added

---
+++ patient_activity_service/src/main/java/com/healthecho/patient_activity_service/PatientActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthecho.patient_activity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientActivityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientActivityServiceApplication.class, args);
+	}
+
+}
---
+++ patient_activity_service/src/test/java/com/healthecho/patient_activity_service/PatientActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthecho.patient_activity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
